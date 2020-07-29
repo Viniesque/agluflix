0996829aed4a0720c9efd36ef3db8199f2888077
Viniesque <viniesque_cpm@hotmail.com>
2020-07-28 22:36:53 -0300

    Commit page 404 e ajuste slide

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import CadastroVideo from './pages/cadastro/Video';
 import CadastroCategoria from './pages/cadastro/Categoria';
-
+import NotFound from './NotFound';
 
 ReactDOM.render(
 
@@ -17,6 +17,7 @@ ReactDOM.render(
       <Route path="/cadastro/video" component={CadastroVideo} />
       <Route path="/cadastro/categoria" component={CadastroCategoria} />
 
+      <Route path="*" component={NotFound}/>
       <Route component={() => (<div>Página 404</div>)} />
     </Switch>
   </BrowserRouter>,
